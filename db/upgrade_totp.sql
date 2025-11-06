a64de814dc908923fba8d7fa69fc9da1da928af2
alosslessdev <ppaanncchhoo507@gmail.com>
2025-11-06 17:56:00 -0500

    fix upgrade

--- db/upgrade_totp.sql
+++ db/upgrade_totp.sql
@@ -1,7 +1,7 @@
 -- Add TOTP fields to users table
-ALTER TABLE `users` 
+ALTER TABLE `dotp_users`
 ADD COLUMN `user_totp_secret` VARCHAR(32) NULL DEFAULT NULL COMMENT 'TOTP secret key',
 ADD COLUMN `user_totp_enabled` TINYINT(1) NOT NULL DEFAULT 0 COMMENT 'Whether TOTP 2FA is enabled';
 
 -- Add index for faster lookups
-CREATE INDEX idx_user_totp ON `users` (user_id, user_totp_enabled);
+CREATE INDEX idx_user_totp ON `dotp_users` (user_id, user_totp_enabled);
